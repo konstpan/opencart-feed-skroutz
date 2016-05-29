4b70653e4422ec313928edab051ada941b285a85
konstpan <konstpan@gmail.com>
2016-05-29 22:03:44 +0300

    Initial commit

---
+++ src/catalog/controller/feed/skroutz.php
@@ -0,0 +1,11 @@
+<?php
+
+class ControllerFeedSkroutz extends Controller {
+  
+  public function index() {
+    $output = 'TBD: products xml feed output';
+    
+    $this->response->setOutput($output);
+  }
+  
+}
