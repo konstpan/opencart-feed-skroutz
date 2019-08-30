c3b70637a3a25906e194cfc763934a4394dfaf52
harrys.revis <hrevisios@gmail.com>
2019-08-30 20:15:58 +0300

    + Fixed file and folder structure ++ Fixed xml mapping +++ Added seo url friendly url in "link" tag

--- src/2.x.x/catalog/controller/extension/feed/skroutz.php
+++ src/2.x.x/catalog/controller/extension/feed/skroutz.php
@@ -6,6 +6,7 @@ class ControllerExtensionFeedSkroutz extends Controller {
     if ($this->config->get('skroutz_status')) {
       $this->load->model('catalog/category');
       $this->load->model('catalog/product');
+      $this->language->load('extension/feed/skroutz');
       
       $output  = '<?xml version="1.0" encoding="utf-8"?>';
       $output .= '<webstore>';
@@ -16,23 +17,20 @@ class ControllerExtensionFeedSkroutz extends Controller {
 
       foreach ($products as $product) {
         $price = $product['special'] ? $product['special'] : $product['price'];
-        $final_vat_price = 
-                $this->tax->calculate($price, $product['tax_class_id'],
-                                      $this->config->get('config_tax'));
+        $final_vat_price = $this->tax->calculate($price, $product['tax_class_id'], $this->config->get('config_tax'));
         
         $in_stock = '';
         $availability = '';
         
         $path = '';
         $path_name = '';
-        
+
         $categories = $this->model_catalog_product->getCategories($product['product_id']);
         
         foreach ($categories as $category) {
           if (!$path) {
             $path .= $category['category_id'];
-          }
-          else {
+          }  else {
             $path .= '_' . $category['category_id'];
           }
           
@@ -40,29 +38,37 @@ class ControllerExtensionFeedSkroutz extends Controller {
                   
           if (!$path_name) {
             $path_name = $category_info['name'];
-          }
-          else {
-            $path_name .= ' - ' . $category_info['name'];
+          } else {
+            $path_name .= ' > ' . $category_info['name'];
           }
         }
+
         if ($product['quantity'] <= 0) {
           $in_stock = 'N';
-          $availability = $product['stock_status'];
-        }
-        else {
+          $availability = $this->language->get('stock_status_' . $this->config->get('skroutz_stock_status_' . $this->getStockStatusId($product['product_id'])));
+        } else {
           $in_stock = 'Y';
-          $availability = 'Άμεση παραλαβή';
+          $availability = $this->language->get('stock_status_1');
         }
                         
         $output .= '<product>';
         $output .= '<id>' . $product['product_id'] . '</id>';
+
         $output .= '<name><![CDATA[' . $product['name'] . ']]></name>';
-        $output .= '<image><![CDATA[' . HTTP_SERVER . 'image/' .$product['image'] . ']]></image>';
-        if ($product['mpn']) $output .= '<mpn>' . $product['mpn'] . '</mpn>';
-        if ($product['ean']) $output .= '<ean>' . $product['ean'] . '</ean>';
+
+        $output .= '<image><![CDATA[' . HTTP_SERVER . 'image/' . $product['image'] . ']]></image>';
+
+        if ($product['mpn']) {
+          $output .= '<mpn>' . $product['mpn'] . '</mpn>';
+        }
+
+        if ($product['ean']) {
+          $output .= '<ean>' . $product['ean'] . '</ean>';
+        }
+
         $output .= '<price_with_vat>' . number_format($final_vat_price, 2, '.', '') . '</price_with_vat>';
         $output .= '<manufacturer><![CDATA[' . $product['manufacturer'] . ']]></manufacturer>';
-        $output .= '<url><![CDATA[' . $this->url->link('product/product', 'path=' . $path . '&product_id=' . $product['product_id']) . ']]></url>';
+        $output .= '<url><![CDATA[' . $this->url->link('product/product', 'product_id=' . $product['product_id']) . ']]></url>';
         $output .= '<category><![CDATA[' . $path_name . ']]></category>';
         $output .= '<instock>' . $in_stock . '</instock>';
         $output .= '<availability>' . $availability . '</availability>';
@@ -75,6 +81,12 @@ class ControllerExtensionFeedSkroutz extends Controller {
       $this->response->addHeader('Content-type: text/xml; charset=utf-8');
       $this->response->setOutput($output);
     }
+  }
+
+  protected function getStockStatusId($product_id){
+    $query = $this->db->query("SELECT prd.stock_status_id AS stock_status_id FROM " . DB_PREFIX . "product prd WHERE prd.product_id = '" . (int)$product_id ."'");
+
+    return $query->row['stock_status_id'];
   }
   
 }
---
+++ src/2.x.x/catalog/language/en-gb/extension/feed/skroutz.php
@@ -0,0 +1,8 @@
+<?php
+    
+// Availability texts
+$_['stock_status_available']    = 'Available in store / Delivery 1 to 3 days';
+$_['stock_status_1']            = 'Available in store / Delivery 1 to 3 days';
+$_['stock_status_2']            = 'Delivery 1 to 3 days';
+$_['stock_status_3']            = 'Delivery 4 to 10 days';
+$_['stock_status_4']            = 'Upon order';
